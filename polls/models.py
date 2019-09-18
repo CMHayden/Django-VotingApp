02f4a5ceb2709b3b426dc76614219fb4994ec611
cmhayden <haydencallum4@gmail.com>
2019-09-18 20:52:02 +0100

    fix typo with def

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    define __str__(self):
+    def __str__(self):
         return self.question_text
 
 class Choice(models.Model):
@@ -12,5 +12,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    define __str__(self):
+    def __str__(self):
         return self.choice_text
